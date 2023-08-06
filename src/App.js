947e8b3a2871848d9a9f8444c0e23503502fc545
midhunrajk <midhunrajk009@gmail.com>
2023-08-06 13:29:37 +0530

    slider

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Mars from './components/Mars';
-import './App.scss';
+import './App.css';
 
 function App() {
     return (
--- src/components/Mars.js
+++ src/components/Mars.js
@@ -10,8 +10,9 @@ const Mars = () => {
     const [sol, setSol] = useState(0);
     const [marsPhoto, setMarsPhoto] = useState(null);
     const [roverData, setRoverData] = useState(null);
-    const [cameraData, setCameraData] = useState({}); 
+    const [cameraData, setCameraData] = useState({});
     const [isLoading, setIsLoading] = useState(true);
+    const [currentIndex, setCurrentIndex] = useState(0); // Moved state to the top level
 
     useEffect(() => {
         const fetchRoverData = async () => {
@@ -23,8 +24,7 @@ const Mars = () => {
                 // console.log(data)
                 setRoverData(data.photo_manifest);
                 setIsLoading(false);
-            } 
-            catch (error) {
+            } catch (error) {
                 console.error('Error fetching rover data:', error);
                 setIsLoading(false);
             }
@@ -32,22 +32,21 @@ const Mars = () => {
         fetchRoverData();
     }, [rover]);
 
-    useEffect(()=>{
-        const fetchCameraData=async()=>{
-            try{
-                const response=await fetch(
+    useEffect(() => {
+        const fetchCameraData = async () => {
+            try {
+                const response = await fetch(
                     `https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=${apiKey}`
-                )
-                const data=await response.json();
+                );
+                const data = await response.json();
                 console.log(data.rovers);
-                setCameraData(data)
+                setCameraData(data);
+            } catch (error) {
+                console.error('Error fetching camera data:', error);
             }
-            catch(error){
-
-            }
-        }
+        };
         fetchCameraData();
-    },[rover])
+    }, [rover]);
 
     useEffect(() => {
         const fetchMarsPhoto = async () => {
@@ -58,21 +57,18 @@ const Mars = () => {
                 const data = await response.json();
                 //   console.log(data);
                 setMarsPhoto(data);
-            } 
-            catch (error) {
+                setCurrentIndex(0); // Reset currentIndex when new photos are fetched
+            } catch (error) {
                 console.error('Error fetching photos:', error);
             }
         };
         fetchMarsPhoto();
     }, [rover, camera, sol]);
 
-
-
-
     const handleRoverChange = (event) => {
         const selectedRover = event.target.value;
         setRover(selectedRover);
-        setCamera(''); 
+        setCamera('');
     };
 
     const handleCameraChange = (event) => {
@@ -94,33 +90,41 @@ const Mars = () => {
         return <option>Select a Rover first</option>;
     };
 
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === 0 ? marsPhoto.photos.length - 1 : prevIndex - 1));
+    };
+
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === marsPhoto.photos.length - 1 ? 0 : prevIndex + 1));
+    };
+
     return (
         <>
-            <Navbar/>
+            <Navbar />
             <div className='main-box'>
                 <h1>Mars Rover Photos</h1>
 
-                    <div className='top-box'>
-                        <label >
-                            Select Rover: 
-                            <select className='select' value={rover} onChange={handleRoverChange}>
-                                <option value="curiosity">Curiosity</option>
-                                <option value="opportunity">Opportunity</option>
-                                <option value="spirit">Spirit</option>
-                            </select>
-                        </label>
-                        <label>
-                            Select Camera:
-                            <select  className='select' value={camera} onChange={handleCameraChange}>
-                                {getCameraOptions()}
-                            </select>
-                        </label>
-                        <label>
-                            Sol (Martian day):
-                            <input  className='select' type="number" value={sol} onChange={(e) => setSol(e.target.value)} />
-                        </label>
-                    </div>
-                    {marsPhoto && marsPhoto.photos && marsPhoto.photos.length > 0 ? (
+                <div className='top-box'>
+                    <label>
+                        Select Rover:
+                        <select className='select' value={rover} onChange={handleRoverChange}>
+                            <option value="curiosity">Curiosity</option>
+                            <option value="opportunity">Opportunity</option>
+                            <option value="spirit">Spirit</option>
+                        </select>
+                    </label>
+                    <label>
+                        Select Camera:
+                        <select className='select' value={camera} onChange={handleCameraChange}>
+                            {getCameraOptions()}
+                        </select>
+                    </label>
+                    <label>
+                        Sol (Martian day):
+                        <input className='select' type="number" value={sol} onChange={(e) => setSol(e.target.value)} />
+                    </label>
+                </div>
+                {marsPhoto && marsPhoto.photos && marsPhoto.photos.length > 0 ? (
                     <div className='detail-box'>
                         <div className='item-details'>
                             <p>Rover Name: {marsPhoto.photos[0].rover.name}</p>
@@ -130,31 +134,27 @@ const Mars = () => {
                             <p>Earth day: {marsPhoto.photos[0].earth_date}</p>
                             <p>Status: {marsPhoto.photos[0].rover.status}</p>
                         </div>
-                        <div className='image'>
-                        {marsPhoto.photos.map((photo) => (
-                            <React.Fragment key={photo.id}>
-                                <div className='image-headings'>
-                                    <h2 className='h2'>
-                                        {photo.camera.full_name}
-                                    </h2>
-                                    <h4 className='h4'>
-                                        {photo.camera.name}
-                                    </h4>
-                                </div>
-                                
-                                <img
-                                    className='img'
-                                    src={photo.img_src}
-                                    alt={`Mars${photo.id}`}
-                                />
-                            </React.Fragment>
-                        ))}      
-                        </div> 
+                        <button className="prev-btn" onClick={handlePrev}>
+                            &lt; 
+                        </button>
+                        <div className="image-slider">
+                            <div className='headings'>
+                            <h2 className='h2'>{marsPhoto.photos[currentIndex].camera.full_name}</h2>
+                            <h4 className='h4'>{marsPhoto.photos[currentIndex].camera.name}</h4>
+                            </div>
+                            <img
+                                className='slider-image'
+                                src={marsPhoto.photos[currentIndex].img_src}
+                                alt={`Mars${marsPhoto.photos[currentIndex].id}`}
+                            />
+                        </div>
+                        <button className="next-btn" onClick={handleNext}>
+                             &gt;
+                        </button>
                     </div>
                 ) : (
                     <p>No Mars photo available</p>
                 )}
-                
             </div>
         </>
     );
